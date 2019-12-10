bc0098629b495f3efd9bc6e8f200c823b55037f4
Barld Boot <bakeds@gmail.com>
2019-12-10 18:04:32 +0100

    add day 2b

--- src/main.kt
+++ src/main.kt
@@ -5,6 +5,7 @@ fun main(args: Array<String>) {
     println("day 1B: " + day1B())
 
     println("day 2A: " + day2A())
+    println("day 2B: " + day2B())
 }
 
 fun day1A(): Int {
@@ -51,4 +52,35 @@ fun day2A(): Int {
     }
 
     return state[0]
+}
+
+fun day2B(): Int {
+    val listState = File("C:\\Users\\Barld Boot\\IdeaProjects\\advent2019\\src\\day2.txt")
+        .readText()
+        .split(',')
+        .map { it.toInt() }
+
+    var state = listState.toIntArray()
+
+    for(noun in 0..99){
+        for(verb in 0..99){
+            state[1] = noun
+            state[2] = verb
+
+            var i = 0
+            while(state[i] != 99){
+                state[state[i+3]] = when {
+                    state[i] == 1 -> state[state[i+1]] + state[state[i+2]]
+                    else -> state[state[i+1]] * state[state[i+2]]
+                }
+                i += 4
+            }
+            if(state[0] == 19690720) return 100 * noun + verb
+            state = listState.toIntArray()
+        }
+    }
+
+
+
+    return 0
 }
